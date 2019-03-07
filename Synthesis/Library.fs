62cd5b533bfc710fab5e00c560e079e6a2a218a2
Freewillie <staplewm@gmail.com>
2019-03-07 15:45:47 +0200

    Left prac at 15:45

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,25 +1,43 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar a =  //Create a function abelar to return true if the input is greater than 12, and less than 3097, and is a multiple of 12.
+    match ((a > 12) && (a < 3097)) && (a % 12 = 0) with | true -> true | _ -> false // [target: 1 line]
+    
 
-let area _ _ =
-    failwith "Not implemented"
+let area a b =  // Create a function area which, given a base and height, finds the area of a triangle. If either the base or the height is negative, throw an exception. 
+    match (a<0.0)||(b<0.0) with             //[target: 3 lines] 
+    | true -> failwith "Cannot be negitive"
+    | _ ->  ((a/2.0)*b)
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo a =           //Create a function zollo which returns a positive number if given a negative number,or doubles the number if the input is positive.
+    match (a<0) with                 //[target: 3 lines]
+    |true -> (a*(-1))
+    |_ -> (a*2)
+    
 
-let min _ _ =
-    failwith "Not implemented"
+let min a b =   //Create a function min which chooses the smaller of two values.
+    match (a<b) with // [target: 3 lines]
+    |true -> a
+    |_ -> b
 
-let max _ _ =
-    failwith "Not implemented"
+let max a b =   //Create a function max which chooses the larger of two values.
+    match (a>b) with // [target: 3 lines]
+    |true -> a
+    |_ -> b
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime h m s = //Create a function ofTime to convert hours, minutes, and seconds to a number of seconds.
+    ((h*60)*60) + (m*60) + (s)                           //[target: 1 line]
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime s =      //Create a function toTime to convert a number of seconds to hours, minutes, and seconds.
+     match (s>0) with   //[target: 7 lines]
+     |false -> 0,0,0
+     |true -> 
+     let h = (s/3600)  
+     let m = ((s-(3600*h))/60)
+     let sec = (s-(3600*h)-(m*60))
+     h,m,sec
+     
+   
 
 let digits _ =
     failwith "Not implemented"
